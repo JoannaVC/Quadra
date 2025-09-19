4db922832ca9a837e7a551db9a88338f77bbfcd9
Joanna Viveros <joanna2409019@hybridge.education>
2025-09-19 12:42:32 -0600

    db: modifica los datos necesarios

--- DB/models.py
+++ DB/models.py
@@ -5,8 +5,6 @@ class Usuario(db.Model):
     __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
     nom_usuario = db.Column(db.String(50), unique=True, nullable=False)
-    nombre = db.Column(db.String(100), nullable=False)
-    foto = db.Column(db.String(200))
     correo = db.Column(db.String(120), unique=True, nullable=False)
     contraseña = db.Column(db.String(128), nullable=False)  # por ahora texto plano
 
