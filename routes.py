8991972324561ee976ea0a97151fda4a026b9b63
Joanna Viveros <joanna2409019@hybridge.education>
2025-09-18 20:57:48 -0600

    Agrega todas las rutas

--- routes.py
+++ routes.py
@@ -4,6 +4,7 @@
 from app import app, db
 from DB.models import Usuario
 from flask import render_template, redirect, url_for
+import os
 
 @app.route('/add_user/<nombre>/<email>')
 def add_user(nombre, email):
@@ -16,3 +17,33 @@ def add_user(nombre, email):
 def usuarios():
     usuarios = Usuario.query.all()
     return render_template('auth/login.html', usuarios=usuarios)
+
+#PRUEBA RENDER TEMPLATE
+@app.route('/')
+def index():
+    return render_template('landing.html')
+
+@app.route('/login')
+def login():
+    return render_template('auth/login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('auth/signup.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('main/dashboard.html')
+
+@app.route('/mapa')
+def mapa():
+    api_key = os.getenv("GEOAPIFY_API_KEY", "8ba67a277ac44467b33467a772a0ca3e")
+    return render_template("main/map.html", api_key=api_key)
+
+@app.route('/foodstand')
+def foodstand():
+    return render_template("foodstand.html", user_is_creator=False)
+
+@app.route('/comments')
+def comments():
+    return render_template("comments.html", user_is_creator=False)
