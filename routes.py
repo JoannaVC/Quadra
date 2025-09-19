92b009f83120b758672074c29a671657e4590daf
Joanna Viveros <joanna2409019@hybridge.education>
2025-09-19 12:44:57 -0600

    feat: agrega funciones para subir datos a la DB

--- routes.py
+++ routes.py
@@ -3,32 +3,47 @@
 #EJEMPLO_PRUEBA_DB
 from app import app, db
 from DB.models import Usuario
+from flask import request
+from flask import session, flash
 from flask import render_template, redirect, url_for
 import os
-
-@app.route('/add_user/<nombre>/<email>')
-def add_user(nombre, email):
-    usuario = Usuario(nombre=nombre, email=email)
-    db.session.add(usuario)
-    db.session.commit()
-    return redirect(url_for('usuarios'))
-
-@app.route('/usuarios')
-def usuarios():
-    usuarios = Usuario.query.all()
-    return render_template('auth/login.html', usuarios=usuarios)
 
 #PRUEBA RENDER TEMPLATE
 @app.route('/')
 def index():
     return render_template('landing.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        # Usa los nombres de columna correctos
+        usuario = Usuario.query.filter_by(correo=email, contraseña=password).first()
+
+        if usuario:
+            session['user_id'] = usuario.id
+            session['user_name'] = usuario.nom_usuario
+            return redirect(url_for('dashboard'))
+        else:
+            flash("Correo o contraseña incorrectos")
+            return redirect(url_for('login'))
+
     return render_template('auth/login.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        email = request.form['email']
+        password = request.form['password']
+        #Agregar a la DB
+        usuario = Usuario(nom_usuario=nombre, correo=email, contraseña=password)
+        db.session.add(usuario)
+        db.session.commit()
+        return redirect(url_for('dashboard'))
+
     return render_template('auth/signup.html')
 
 @app.route('/dashboard')
